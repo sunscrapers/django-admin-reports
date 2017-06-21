598c25d4a0c71c07aa0d394b61094b98d6a682d9
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-06-21 15:57:43 +0200

    Bail out custom register
    Update __init__.py

--- admin_reports/__init__.py
+++ admin_reports/__init__.py
@@ -1,14 +1,7 @@
-from django.utils.module_loading import autodiscover_modules
-from .decorators import register
-from .sites import AdminReportSite, site
 from .reports import Report
 
 __version__ = '0.10.4'
-__all__ = ["register", "AdminReportSite", "site", "Report"]
-
-
-def autodiscover():
-    autodiscover_modules('reports', register_to=site)
+__all__ = ['Report']
 
 
 default_app_config = 'admin_reports.apps.AdminReportConfig'
--- admin_reports/decorators.py
+++
@@ -1,6 +0,0 @@
-def register():
-    from .sites import site
-    def _report_wrapper(report_class):
-        site.register(report_class)
-        return report_class
-    return _report_wrapper
